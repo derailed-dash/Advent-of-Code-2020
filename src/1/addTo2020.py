8a8bf876e6e2b60e7a416666db3c710d929d183e
Dazbo <derailed.dash@gmail.com>
2020-12-22 15:56:06 +0000

    Using itertools combinatios for a generic solution

--- src/1/addTo2020.py
+++ src/1/addTo2020.py
@@ -15,42 +15,42 @@ import time
 def read_input(a_file):
     with open(a_file, mode="rt") as f:
         global entries_list
-        entries_list = f.read().splitlines()
+        entries_list = [int(x) for x in f.read().splitlines()]
 
-def total_of_two():
+
+def total_of_two(target):
     iterations = 0
-    for i in range(len(entries_list)):
-        for j in range(i+1, len(entries_list)):
+    for i, num1 in enumerate(entries_list):
+        for num2 in entries_list[i+1:]:
             iterations += 1
-            the_sum = int(entries_list[i]) + int(entries_list[j])
+            the_sum = num1 + num2
             if (the_sum == target):
-                print(f"The sum of {entries_list[i]} and {entries_list[j]} is: " + str(the_sum))
-                print(f"And the product is: " + str(int(entries_list[i])*int(entries_list[j])))
+                print(f"The sum of {num1} and {num2} is: " + str(target))
+                print(f"And the product is: " + str(num1* num2))
                 print(f"{iterations} iterations required.")
 
 
-def total_of_three():
+def total_of_three(target):
     iterations = 0
-    for i in range(len(entries_list)):
-        for j in range(i+1, len(entries_list)):
+    for i, num1 in enumerate(entries_list):
+        for j, num2 in enumerate(entries_list[i+1:]):
             iterations += 1
-            sum_so_far = int(entries_list[i]) + int(entries_list[j])
+            sum_so_far = num1 + num2
             if (sum_so_far >=target):
                 # we've already exceeded target with just two numbers
                 continue
             
-            for k in range(j+1, len(entries_list)):
+            for num3 in entries_list[j+1:]:
                 iterations += 1
-                the_sum = sum_so_far + int(entries_list[k])
+                the_sum = sum_so_far + num3
                 if (the_sum == target):
-                    print(f"The sum of {entries_list[i]} and {entries_list[j]} and {entries_list[k]} is: " + str(the_sum))
-                    print(f"And the product is: " + str(int(entries_list[i])*int(entries_list[j])*int(entries_list[k])))
+                    print(f"The sum of {num1} and {num2} and {num3} is: " + str(the_sum))
+                    print(f"And the product is: " + str(num1*num2*num3))
                     print(f"{iterations} iterations required.")
                     return
 
 
 target = 2020
-the_sum = 0
 
 # get absolute path where script lives
 script_dir = os.path.dirname(__file__) 
@@ -64,13 +64,13 @@ print("File read.")
 # print(entries_list)
 print("Looking for two numbers that total " + str(target))
 t1 = time.perf_counter()
-total_of_two()
+total_of_two(target)
 t2 = time.perf_counter()
 print(f"Execution time: {t2 - t1:0.4f} seconds")
 
 print("Looking for three numbers that total " + str(target))
 t1 = time.perf_counter()
-total_of_three()
+total_of_three(target)
 t2 = time.perf_counter()
 print(f"Execution time: {t2 - t1:0.4f} seconds")
 
---
+++ src/1/add_to_2020_with_combinations.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import time
+import re
+from itertools import combinations
+from math import prod as prod
+
+INPUT_FILE = "input/expenses.txt"
+
+def main():
+    # get absolute path where script lives
+    script_dir = os.path.dirname(__file__) 
+    print("Script location: " + script_dir)
+
+    # path of input file
+    input_file = os.path.join(script_dir, INPUT_FILE)
+    print("Input file is: " + input_file)
+    entries = read_input(input_file)
+    print(entries)
+
+    target = 2020
+    determine_terms(entries, target, 2)
+    determine_terms(entries, target, 3)
+
+
+def determine_terms(entries, target, num_terms):
+    for num_list in combinations(entries, num_terms):
+        the_sum = sum([*num_list])
+        if the_sum == target:
+            print(f"Terms: {num_list}.")
+            print(f"And the product is: " + str(prod(num_list)))
+            break
+
+
+def read_input(a_file):
+    with open(a_file, mode="rt") as f:
+        entries_list = [int(x) for x in f.read().splitlines()]
+
+    return entries_list
+
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main()
+    t2 = time.perf_counter()
+    print(f"Execution time: {t2 - t1:0.4f} seconds")
